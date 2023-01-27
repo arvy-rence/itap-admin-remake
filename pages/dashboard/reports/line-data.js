b89153963a9c375a358f9ac11aa1b6576fdc244c
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2023-01-27 22:24:32 +0800

    Add sample data for charts

---
+++ pages/dashboard/reports/line-data.js
@@ -0,0 +1,131 @@
+const data = [
+  {
+    id: 'Male',
+    color: '#00104A',
+    data: [
+      {
+        x: 'JAN',
+        y: 23,
+      },
+      {
+        x: 'FEB',
+        y: 45,
+      },
+      {
+        x: 'MAR',
+        y: 112,
+      },
+      {
+        x: 'APR',
+        y: 80,
+      },
+      {
+        x: 'MAY',
+        y: 99,
+      },
+      {
+        x: 'JUN',
+        y: 103,
+      },
+      {
+        x: 'JUL',
+        y: 76,
+      },
+      {
+        x: 'AUG',
+        y: 90,
+      },
+      {
+        x: 'SEP',
+        y: 139,
+      },
+      {
+        x: 'OCT',
+        y: 136,
+      },
+      {
+        x: 'NOV',
+        y: 54,
+      },
+      {
+        x: 'DEC',
+        y: 136,
+      },
+    ],
+  },
+  {
+    id: 'Female',
+    color: '#f796a7',
+    data: [
+      {
+        x: 'JAN',
+        y: 13,
+      },
+      {
+        x: 'FEB',
+        y: 35,
+      },
+      {
+        x: 'MAR',
+        y: 111,
+      },
+      {
+        x: 'APR',
+        y: 99,
+      },
+      {
+        x: 'MAY',
+        y: 130,
+      },
+      {
+        x: 'JUN',
+        y: 70,
+      },
+      {
+        x: 'JUL',
+        y: 50,
+      },
+      {
+        x: 'AUG',
+        y: 60,
+      },
+      {
+        x: 'SEP',
+        y: 139,
+      },
+      {
+        x: 'OCT',
+        y: 76,
+      },
+      {
+        x: 'NOV',
+        y: 87,
+      },
+      {
+        x: 'DEC',
+        y: 113,
+      },
+    ],
+  },
+]
+
+export default data
+
+
+/**
+ * 
+ * shape of data
+ * [
+ *  {
+ *   "id": "legend name",
+ *   "color": "color",
+ *   "data": [
+ *    {
+ *     "x": "x axis name",
+ *     "y": y-value-int
+ *    }, ..., {}
+ *   ]
+ *  }
+ * ]
+ * 
+ */
---
+++ pages/dashboard/reports/pie-data.js
@@ -0,0 +1,34 @@
+const data = [
+  {
+    id: 'Children (0-14)',
+    label: 'Children',
+    value: 379,
+    color: '#80c993',
+  },
+  {
+    id: 'Early Working Age (15-24)',
+    label: 'Early Working Age (15-24)',
+    value: 297,
+    color: '#019328',
+  },
+  {
+    id: 'Prime Working Age (25-54)',
+    label: 'Prime Working Age (25-54)',
+    value: 390,
+    color: '#8087a4',
+  },
+  {
+    id: 'Mature Working Age (55-64)',
+    label: 'Mature Working Age (55-64)',
+    value: 534,
+    color: '#00104a',
+  },
+  {
+    id: 'Elderly (65 and above)',
+    label: 'Elderly (65 and above)',
+    value: 300,
+    color: '#e10909',
+  },
+]
+
+export default data
