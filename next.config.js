fa10e78a6bdbd85a0611badc9a9cbfbaf337c5b9
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2023-01-27 21:53:07 +0800

    Add redirect for /dashboard

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  redirects: async () => {
+    return [
+      {
+        source: '/dashboard',
+        destination: '/dashboard/reports',
+        permanent: true,
+      },
+    ]
+  }
 }
